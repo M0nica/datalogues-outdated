4c124ffa2a8bbc7e57895dcab7d88f25b50d7a00
Monica Powell <mmp2181@columbia.edu>
2017-05-20 10:32:42 -0400

    configured base of the blog and wrote 2 test articles

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,15 +26,28 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+SOCIAL = (
+        ('Twitter', 'http://twitter.com/waterproofheart'),
+        ('Email', 'mailto:monica@aboutmonica.com'),
+        ('GitHub', 'https://github.com/M0nica'),
+          )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+# render ipynb as markdown
 MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATH = './plugins'
 PLUGINS = ['ipynb.markup']
+
+# pelican will ignore ipynb checkpoints
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# import pelican-theme
+THEME = "../pelican-themes/gum"
