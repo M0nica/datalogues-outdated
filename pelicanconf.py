b1b10ecf4d51b2823fbf4d0d2dee8ef57437305c
Monica Powell <mmp2181@columbia.edu>
2017-05-20 09:14:19 -0400

    added plugin to render python notebooks in pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,8 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATH = './plugins'
+PLUGINS = ['ipynb.markup']
